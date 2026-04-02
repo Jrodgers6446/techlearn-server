e943667aff6f5a0dad1241fb38afd06d15e1088f
J rodgers <J.rodgers@ubreakifix.com>
2026-04-02 14:42:31 -0400

    add error logging to catch startup crash

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err.message);
+  console.error(err.stack);
+  process.exit(1);
+});
+process.on('unhandledRejection', (reason) => {
+  console.error('UNHANDLED REJECTION:', reason);
+  process.exit(1);
+});
+
 const express    = require('express');
 const cors       = require('cors');
 const { Pool }   = require('pg');
